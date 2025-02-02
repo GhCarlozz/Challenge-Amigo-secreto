7c0f4cf02ab3a8b4dc7dda7b4512b81b538b3f83
GhCarlozz <carlosrcastil@gmail.com>
2025-02-01 18:56:51 -0600

    Se implementó la función

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    //capturar el valor de entrada
+    let nombre = document.getElementById("amigo").value;
+    //se valida que el campo no esté vacío
+    if(nombre.trim() === ""){
+        alert("Por favor, inserte un nombre.");
+        console.log(nombre);
+        
+    } else {
+        //se agrega el nombre al array amigos
+        amigos.push(nombre);
+        //Se limpia la caja de texto por una cadena vacía
+        document.getElementById("amigo").value = "";
+        console.log(amigos);
+
+    }
+}
+
+
+
+
